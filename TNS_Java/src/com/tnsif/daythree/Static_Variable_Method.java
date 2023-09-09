64b39c90810db24d6173cd3fdfbfdf967a2d2b7a
Admin <Admin@DESKTOP-STGDSSD>
2023-09-09 23:34:45 +0530

    2 commit

--- TNS_Java/src/com/tnsif/daythree/Static_Variable_Method.java
+++ TNS_Java/src/com/tnsif/daythree/Static_Variable_Method.java
@@ -3,5 +3,6 @@ public class Static_Variable_Method {
 public static void main(String[] args) {
 	System.out.println(Cube.x);
 	System.out.println("Cube value of 5 is: "+Cube.calculateCube(5));
+	//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
 }
 }
