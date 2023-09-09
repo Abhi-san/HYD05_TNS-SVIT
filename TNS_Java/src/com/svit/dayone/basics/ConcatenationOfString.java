8e555a9fc4b114bec0b5c49c05adc8769b2a41e0
Admin <Admin@DESKTOP-STGDSSD>
2023-09-09 23:07:41 +0530

    commit

---
+++ TNS_Java/src/com/svit/dayone/basics/ConcatenationOfString.java
@@ -0,0 +1,10 @@
+package com.svit.dayone.basics;
+
+public class ConcatenationOfString {
+	public static void main(String[] args) {    
+		   String var1 = "10";    
+		    String var2 = "20";    
+		    System.out.println(var1 + var2);    
+		}
+
+}
---
+++ TNS_Java/src/com/svit/dayone/basics/VariableOperations.java
@@ -0,0 +1,14 @@
+package com.svit.dayone.basics;
+
+public class VariableOperations {
+	public static void main(String[] args) {
+		   int value1 = 1, value2 = 2, value3 = (2 * 2) - 1;
+		      int value4 = 4; 
+		      int value5 = value4;  
+			System.out.println(value1);
+		   System.out.println (value2);
+		   System.out.println (value3);
+		   System.out.println (value5);  
+		 }
+
+}
